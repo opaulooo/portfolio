4bcf715a5f70f9b81c5e4a1f0141c53f8501a886
opaulooo <o.paulooo.victor@gmail.com>
2026-01-02 01:11:21 -0300

    wip: improve portfolio sctructure

---
+++ src/js/technologies.js
@@ -0,0 +1,94 @@
+// Array de tecnologias
+const technologies = [
+    {
+        name: 'Angular',
+        src: 'src/assets/icons/techs/angular.svg',
+        width: '50%'
+    },
+    {
+        name: 'IONIC',
+        src: 'src/assets/icons/techs/ionic.svg',
+        width: '50%'
+    },
+    {
+        name: '.NET',
+        src: 'src/assets/icons/techs/dotnet.svg',
+        width: '50%'
+    },
+    {
+        name: 'MySQL',
+        src: 'src/assets/icons/techs/mysql.svg',
+        width: '50%'
+    },
+    {
+        name: 'Docker',
+        src: 'src/assets/icons/techs/docker.svg',
+        width: '50%'
+    },
+    {
+        name: 'AWS',
+        src: 'src/assets/icons/techs/aws.svg',
+        width: '50%'
+    },
+    {
+        name: 'NodeJS',
+        src: 'src/assets/icons/techs/nodejs.svg',
+        width: '60%'
+    },
+    {
+        name: 'React',
+        src: 'src/assets/icons/techs/react.svg',
+        width: '50%'
+    },
+    {
+        name: 'Nest.js',
+        src: 'src/assets/icons/techs/nestjs.svg',
+        width: '50%'
+    },
+    {
+        name: 'JAVA',
+        src: 'src/assets/icons/techs/java.svg',
+        width: '50%'
+    },
+    {
+        name: 'Python',
+        src: 'src/assets/icons/techs/python.svg',
+        width: '50%'
+    }
+];
+
+// Função para renderizar as tecnologias
+function renderTechnologies() {
+    const container = document.getElementById('technologies-grid');
+    
+    if (!container) return;
+    
+    let currentRow;
+    
+    technologies.forEach((tech, index) => {
+        // Cria uma nova linha a cada 3 itens
+        if (index % 3 === 0) {
+            currentRow = document.createElement('div');
+            currentRow.className = 'row card-body';
+            container.appendChild(currentRow);
+        }
+        
+        // Cria o elemento da tecnologia
+        const techItem = document.createElement('div');
+        techItem.className = 'col-lg-4 col-xl-4 align-middle';
+        techItem.style.marginTop = '1em';
+        
+        techItem.innerHTML = `
+            <div class="text-center text-uppercase text-secondary">
+                <a data-toggle="tooltip" data-placement="top" title="${tech.name}">
+                    <img class="techs" src="${tech.src}" alt="${tech.name}" 
+                         width="${tech.width}" style="max-width: 100px;" />
+                </a>
+            </div>
+        `;
+        
+        currentRow.appendChild(techItem);
+    });
+}
+
+// A função será chamada pelo components-loader após o carregamento dos componentes
---
+++ src/js/translations.js
@@ -0,0 +1,229 @@
+const translations = {
+    pt: {
+        // Navigation
+        navMenu: "Menu",
+        navAbout: "Sobre Mim",
+        navTechs: "Tecnologias",
+        navHobby: "Hobbies",
+        navPortfolio: "Portfolio",
+        navToggle: "Menu",
+        
+        // Header
+        name: "Paulo Victor",
+        technologies: ['Angular', '.NET', 'IONIC', 'Node', 'Express'],
+        
+        // About Section
+        aboutTitle: "Sobre mim",
+        aboutText1: `Me chamo Paulo Victor, tenho 25 anos e sou formado em Engenharia da Computação pelo
+            IFTM – Campus Uberaba Parque Tecnológico. Sou desenvolvedor Full Stack, atualmente trabalho usando Angular + IONIC, .NET
+            e MySQL, mas tenho me desafiado estudando um pouco React e Nest.js. Também tenho procurado me aprimorar em Docker e Padrões de Arquitetura.`,
+        aboutText2: `
+            Atuo como Desenvolvedor Full Stack há aproximadamente 4 anos, com experiência no
+            desenvolvimento de aplicações utilizando Angular, Ionic, .NET e Express, além de
+            bancos de dados relacionais e não relacionais, como MySQL e MongoDB.<br>Também trabalho com mensageria via AWS SQS, logs centralizados com OpenSearch,<br>monitoramento de aplicações com New Relic e testes unitários, de integração e de carga,
+            utilizando xUnit e JMeter.`,
+        
+        // Technologies Section
+        techsTitle: "Tecnologias",
+        techsText1: "Essas são algumas das tecnologias que possuo experiência e que estou estudando para me aprimorar, gosto bastante com o Front-End, mas nos últimos meses tenho me interessado cada vez mais pelo Back-End e Mobile.",
+        techsText2: "Tenho mais experiência com Angular, Node, IONIC e SQLite, onde trabalhei desenvolvendo um sistema WEB com a API e App interagindo entre si para a logística do controle de estoque dos clientes da empresa.",
+        techsText3: "Ultimamente estou gostando de aprender sobre as outras tecnologias listadas, que parecem ser bem legais e bastante usadas no mercado.",
+        
+        // Hobbies Section
+        hobbyTitle: "Hobbies",
+        hobbyText: "Gosto bastante de aprender novas tecnologias e aprimorar as que já tive contato, como o próprio Angular, IONIC e NodeJS. Fora dessa linha, também sou um bom consumidor de animes e mangás, jogos online, como CS: GO, Fortnite, GTA, Valorant, APEX, R6, entre alguns outros que jogo bastante com meus amigos. E outro totalmente fora da curva, também gosto de arranhar meu violino e ouvir umas músicas clássicas (principalmente com solista violino).",
+        
+        // Portfolio Section
+        portfolioTitle: "Portfólio",
+        
+        // Footer
+        footerSocial: "Redes Sociais",
+        footerResumeBtn: "Confira meu Currículo!",
+        footerRights: "Direitos Reservados a"
+    },
+    en: {
+        // Navigation
+        navMenu: "Menu",
+        navAbout: "About Me",
+        navTechs: "Technologies",
+        navHobby: "Hobbies",
+        navPortfolio: "Portfolio",
+        navToggle: "Menu",
+        
+        // Header
+        name: "Paulo Victor",
+        technologies: ['Angular', '.NET', 'IONIC', 'Node', 'Express'],
+        
+        // About Section
+        aboutTitle: "About Me",
+        aboutText1: `My name is Paulo Victor, I am 25 years old and I hold a degree in Computer Engineering from
+            IFTM – Uberaba Parque Tecnológico Campus. I am a Full Stack Developer, currently working with
+            Angular + Ionic, .NET and MySQL. I have also been challenging myself by studying React and
+            Nest.js, in addition to improving my skills in Docker and software architecture patterns.`,
+        aboutText2: `I have approximately 4 years of professional experience as a Full Stack Developer, working on
+            application development using Angular, Ionic, .NET and Express, as well as relational and
+            non-relational databases such as MySQL and MongoDB.<br>I also work with messaging using AWS SQS, centralized logging with OpenSearch, application monitoring with New Relic, and unit, integration and load testing using xUnit and JMeter.`,
+
+        
+        // Technologies Section
+        techsTitle: "Technologies",
+        techsText1: "These are some of the technologies I have experience with and that I'm studying to improve, I really like Front-End, but in recent months I've become increasingly interested in Back-End and Mobile.",
+        techsText2: "I have more experience with Angular, Node, IONIC and SQLite, where I worked developing a WEB system with API and App interacting with each other for the logistics of stock control of company clients.",
+        techsText3: "Lately I've been enjoying learning about the other listed technologies, which seem to be quite cool and widely used in the market.",
+        
+        // Hobbies Section
+        hobbyTitle: "Hobbies",
+        hobbyText: "I really like learning new technologies and improving the ones I've already had contact with, like Angular, IONIC and NodeJS. Outside of this line, I'm also a good consumer of anime and manga, online games, like CS: GO, Fortnite, GTA, Valorant, APEX, R6, among some others that I play a lot with my friends. And another one totally out of the curve, I also like to scratch my violin and listen to some classical music (especially with violin soloist).",
+        
+        // Portfolio Section
+        portfolioTitle: "Portfolio",
+        
+        // Footer
+        footerSocial: "Social Networks",
+        footerResumeBtn: "Check out my Resume!",
+        footerRights: "All Rights Reserved to"
+    },
+    es: {
+        // Navigation
+        navMenu: "Menú",
+        navAbout: "Sobre Mí",
+        navTechs: "Tecnologías",
+        navHobby: "Pasatiempos",
+        navPortfolio: "Portafolio",
+        navToggle: "Menú",
+        
+        // Header
+        name: "Paulo Victor",
+        technologies: ['Angular', '.NET', 'IONIC', 'Node', 'Express'],
+        
+        // About Section
+        aboutTitle: "Sobre mí",
+        aboutText1: `Me llamo Paulo Victor, tengo 25 años y soy graduado en Ingeniería de la Computación por el
+        IFTM – Campus Uberaba Parque Tecnológico. Soy Desarrollador Full Stack y actualmente trabajo
+        con Angular + Ionic, .NET y MySQL. Además, me he estado desafiando a estudiar React y Nest.js,
+        así como a mejorar mis conocimientos en Docker y patrones de arquitectura de software.`,
+
+        aboutText2: `Cuento con aproximadamente 4 años de experiencia profesional como Desarrollador Full Stack,
+        trabajando en el desarrollo de aplicaciones con Angular, Ionic, .NET y Express, además de
+        bases de datos relacionales y no relacionales como MySQL y MongoDB.<br>También trabajo con mensajería mediante AWS SQS, registros centralizados con OpenSearch, monitoreo de aplicaciones con New Relic y pruebas unitarias, de integración y de carga,
+        utilizando xUnit y JMeter.`,
+        
+        // Technologies Section
+        techsTitle: "Tecnologías",
+        techsText1: "Estas son algunas de las tecnologías con las que tengo experiencia y que estoy estudiando para mejorar, me gusta mucho el Front-End, pero en los últimos meses me he interesado cada vez más por el Back-End y Mobile.",
+        techsText2: "Tengo más experiencia con Angular, Node, IONIC y SQLite, donde trabajé desarrollando un sistema WEB con API y App interactuando entre sí para la logística del control de stock de los clientes de la empresa.",
+        techsText3: "Últimamente me ha gustado aprender sobre las otras tecnologías listadas, que parecen ser bastante interesantes y muy utilizadas en el mercado.",
+        
+        // Hobbies Section
+        hobbyTitle: "Pasatiempos",
+        hobbyText: "Me gusta mucho aprender nuevas tecnologías y mejorar las que ya he tenido contacto, como Angular, IONIC y NodeJS. Fuera de esta línea, también soy un buen consumidor de anime y manga, juegos en línea, como CS: GO, Fortnite, GTA, Valorant, APEX, R6, entre algunos otros que juego mucho con mis amigos. Y otro totalmente fuera de la curva, también me gusta arañar mi violín y escuchar música clásica (especialmente con solista de violín).",
+        
+        // Portfolio Section
+        portfolioTitle: "Portafolio",
+        
+        // Footer
+        footerSocial: "Redes Sociales",
+        footerResumeBtn: "¡Consulta mi Currículum!",
+        footerRights: "Todos los Derechos Reservados a"
+    }
+};
+
+// Idioma padrão
+let currentLanguage = 'pt';
+
+// Função para aplicar traduções
+function applyTranslations(lang) {
+    currentLanguage = lang;
+    const t = translations[lang];
+    
+    // Navigation
+    document.querySelectorAll('[data-i18n="navMenu"]').forEach(el => el.innerHTML = t.navMenu);
+    document.querySelectorAll('[data-i18n="navAbout"]').forEach(el => el.innerHTML = t.navAbout);
+    document.querySelectorAll('[data-i18n="navTechs"]').forEach(el => el.innerHTML = t.navTechs);
+    document.querySelectorAll('[data-i18n="navHobby"]').forEach(el => el.innerHTML = t.navHobby);
+    document.querySelectorAll('[data-i18n="navPortfolio"]').forEach(el => el.innerHTML = t.navPortfolio);
+    document.querySelectorAll('[data-i18n="navToggle"]').forEach(el => el.innerHTML = t.navToggle);
+    
+    // Header
+    const nameEl = document.querySelector('[data-i18n="name"]');
+    if (nameEl) nameEl.innerHTML = t.name;
+    
+    // Tecnologias do header (já existe a função renderTechnologies)
+    const techContainer = document.querySelector('.masthead-subheading');
+    if (techContainer) {
+        techContainer.innerHTML = t.technologies.join('&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;');
+    }
+    
+    // About Section
+    const aboutTitle = document.querySelector('[data-i18n="aboutTitle"]');
+    if (aboutTitle) aboutTitle.innerHTML = t.aboutTitle;
+    
+    const aboutText1 = document.querySelector('[data-i18n="aboutText1"]');
+    if (aboutText1) aboutText1.innerHTML = t.aboutText1;
+    
+    const aboutText2 = document.querySelector('[data-i18n="aboutText2"]');
+    if (aboutText2) aboutText2.innerHTML = t.aboutText2;
+    
+    // Technologies Section
+    const techsTitle = document.querySelector('[data-i18n="techsTitle"]');
+    if (techsTitle) techsTitle.innerHTML = t.techsTitle;
+    
+    const techsText1 = document.querySelector('[data-i18n="techsText1"]');
+    if (techsText1) techsText1.innerHTML = t.techsText1;
+    
+    const techsText2 = document.querySelector('[data-i18n="techsText2"]');
+    if (techsText2) techsText2.innerHTML = t.techsText2;
+    
+    const techsText3 = document.querySelector('[data-i18n="techsText3"]');
+    if (techsText3) techsText3.innerHTML = t.techsText3;
+    
+    // Hobbies Section
+    const hobbyTitle = document.querySelector('[data-i18n="hobbyTitle"]');
+    if (hobbyTitle) hobbyTitle.innerHTML = t.hobbyTitle;
+    
+    const hobbyText = document.querySelector('[data-i18n="hobbyText"]');
+    if (hobbyText) hobbyText.innerHTML = t.hobbyText;
+    
+    // Portfolio Section
+    const portfolioTitle = document.querySelector('[data-i18n="portfolioTitle"]');
+    if (portfolioTitle) portfolioTitle.innerHTML = t.portfolioTitle;
+    
+    // Footer
+    const footerSocial = document.querySelector('[data-i18n="footerSocial"]');
+    if (footerSocial) footerSocial.innerHTML = t.footerSocial;
+    
+    const footerResumeBtn = document.querySelector('[data-i18n="footerResumeBtn"]');
+    if (footerResumeBtn) footerResumeBtn.innerHTML = t.footerResumeBtn;
+    
+    const footerRights = document.querySelector('[data-i18n="footerRights"]');
+    if (footerRights) footerRights.innerHTML = t.footerRights;
+    
+    // Atualizar o select de idioma
+    const languageSelect = document.querySelector('.language-selection');
+    if (languageSelect) {
+        languageSelect.value = lang;
+    }
+    
+    // Salvar preferência no localStorage
+    localStorage.setItem('preferredLanguage', lang);
+}
+
+// Função para trocar idioma
+function changeLanguage(lang) {
+    if (translations[lang]) {
+        applyTranslations(lang);
+    }
+}
+
+// Inicializar com idioma salvo ou padrão
+function initializeLanguage() {
+    const savedLang = localStorage.getItem('preferredLanguage') || 'pt';
+    applyTranslations(savedLang);
+}
+
+// Exportar funções para uso global
+if (typeof window !== 'undefined') {
+    window.changeLanguage = changeLanguage;
+    window.applyTranslations = applyTranslations;
+    window.translations = translations;
+}
